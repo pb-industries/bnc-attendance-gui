3e7a7ef1e71be4447ec39344f934904e6113a549
alexmk92 <alexander.sims92@gmail.com>
2022-04-30 20:58:33 +0100

    Reset on modal close

--- app/components/raids/generateLottoRangeModal.tsx
+++ app/components/raids/generateLottoRangeModal.tsx
@@ -32,6 +32,7 @@ const GenerateLottoRangeModal: FC<HandleLottoRangeModalProps> = ({
   useEffect(() => {
     if (!open) {
       setShowRange(false);
+      setSelectedPlayers(new Set<player>());
     }
   }, [open]);
 
@@ -191,7 +192,10 @@ const GenerateLottoRangeModal: FC<HandleLottoRangeModalProps> = ({
                   <button
                     type="submit"
                     className="inline-flex w-full justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
-                    onClick={() => setShowRange(false)}
+                    onClick={() => {
+                      setShowRange(false);
+                      setSelectedPlayers(new Set());
+                    }}
                   >
                     New Range
                   </button>
